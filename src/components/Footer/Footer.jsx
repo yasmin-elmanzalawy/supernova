2fb45739d0e2e68e66ba3f231b0af27c20673f42
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2025-05-03 10:41:00 +0300

    footer shooting stars

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,89 @@
-import React from 'react';
-import footerbg from "../../assets/footer.png"
-import logo from "../../assets/white-logo.png"
-import tiktok from "../../assets/tiktoppng.png"
-import fb from "../../assets/fb.png"
-import yt from "../../assets/yt.png"
-import discord from "../../assets/discord.png"
-import linkedin from "../../assets/linkden.png"
+import React from "react";
+import footerbg from "../../assets/footer.png";
+import logo from "../../assets/white-logo.png";
+import tiktok from "../../assets/tiktoppng.png";
+import fb from "../../assets/fb.png";
+import ig from "../../assets/ig.png";
+import yt from "../../assets/yt.png";
+import discord from "../../assets/discord.png";
+import linkedin from "../../assets/linkden.png";
+import star from "../../assets/semiministar.png";
+
 
 export default function Footer() {
+
+  const stars = Array.from({ length: 10 }, (_, i) => ({
+    top: Math.random() * 700,
+    left: Math.random() * window.innerWidth,
+    delay: Math.random() * 2.5,
+  }));
+
+
   return (
-    <div className="relative bg-black pt-24">
-    <div className="relative z-50 text-white p-4 flex-wrap md:flex-nowrap flex justify-center">
-      <div className=' md:w-[30%] text-center my-12'> 
-        <h3 className='text-3xl font-extrabold'>Supernova</h3>
-        <p className='py-12 text-xl'>PARLLEL UNIVARESE FOR GAMERS</p>
-        <img className='m-auto' src={logo} alt="white logo" />
-      </div>
-      <div className=' md:w-[30%] text-center my-12'>
-
-        <h3 className='text-3xl font-extrabold '>Contact Information</h3>
-        <p className='py-12 text-xl '> shadyyasser@supernova-gaming.com</p>
-        <p className='text-xl'>+20 1000698166 </p>
-      </div>
-      <div className='md:w-[20%] text-center my-12'>
-        <h3 className='text-3xl font-extrabold '>Follow Us</h3>
-        <div className='flex justify-center '>
-            <div className='md:me-[-60px]'><img className='w-full block' src={fb} alt="facebook icon" /></div>
-            <div className='md:me-[-60px]'><img className='w-full block' src={fb} alt="instagram icon" /></div>
-            <div className='md:me-[-60px]'><img className='w-full block' src={tiktok} alt="tiktok icon" /></div>
+    <div className="relative bg-black pt-24 overflow-hidden">
+           {/* Shooting stars */}
+              {stars.map((starItem, index) => (
+                <img
+                  key={index}
+                  src={star}
+                  alt="star"
+                  className="star"
+                  style={{
+                    top: `${starItem.top}px`,
+                    left: `${starItem.left}px`,
+                    animationDelay: `${starItem.delay}s`,
+                  }}
+                />
+              ))}
+      <div className="relative z-50 text-white p-4 flex-wrap md:flex-nowrap flex justify-center">
+        <div className=" md:w-[30%] text-center my-12">
+          <h3 className="text-3xl font-extrabold">Supernova</h3>
+          <p className="py-12 text-xl">PARLLEL UNIVARESE FOR GAMERS</p>
+          <img className="m-auto" src={logo} alt="white logo" />
         </div>
-        <div className='flex mt-[-50px]'>
-            <div className='md:me-[-60px]'><img className='w-full block' src={linkedin} alt="linkedin icon" /></div>
-            <div className='md:me-[-60px]'><img className='w-full block' src={yt} alt="youtibe icon" /></div>
-            <div className='md:me-[-60px]'><img className='w-full block' src={discord} alt="discord icon" /></div>
+        <div className=" md:w-[30%] text-center my-12">
+          <h3 className="text-3xl font-extrabold ">Contact Information</h3>
+          <p className="py-12 text-xl "> shadyyasser@supernova-gaming.com</p>
+          <p className="text-xl">+20 1000698166 </p>
         </div>
-       
+        <div className="md:w-[20%] text-center my-12">
+          <h3 className="text-3xl font-extrabold ">Follow Us</h3>
+          <div className="flex justify-center ">
+            <div className="md:me-[-60px]">
+              <img className="w-full block" src={fb} alt="facebook icon" />
+            </div>
+            <div className="md:me-[-60px]">
+              <img className="w-full block" src={ig} alt="instagram icon" />
+            </div>
+            <div className="md:me-[-60px]">
+              <img className="w-full block" src={tiktok} alt="tiktok icon" />
+            </div>
+          </div>
+          <div className="flex mt-[-50px]">
+            <div className="md:me-[-60px]">
+              <img
+                className="w-full block"
+                src={linkedin}
+                alt="linkedin icon"
+              />
+            </div>
+            <div className="md:me-[-60px]">
+              <img className="w-full block" src={yt} alt="youtibe icon" />
+            </div>
+            <div className="md:me-[-60px]">
+              <img className="w-full block" src={discord} alt="discord icon" />
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div className="absolute top-[-600px] md:top-0 bottom-0 left-0 right-0 z-10">
+        <img
+          src={footerbg}
+          alt="Color vector"
+          className="w-full h-[100%] object-cover"
+        />
       </div>
     </div>
-  
-    <div className="absolute bottom-0 left-0 right-0 z-10">
-      <img
-        src={footerbg}
-        alt="Color vector"
-        className="w-full h-[100%] object-cover"
-      />
-    </div>
-  </div>
-  
-  )
+  );
 }
