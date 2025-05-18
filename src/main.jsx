54eb8f8797594e2a8eec1586a4d0e43e49bebf16
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2025-05-18 15:02:09 +0300

    edit cname

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
