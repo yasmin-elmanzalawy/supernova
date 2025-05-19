45995857e397b918fb5d90d4ab46b14369b6a8e3
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2025-05-19 22:31:24 +0300

    gg

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
