e8c4a7d6b2360dfb59f7b860e3a4c2ae26c294a6
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2025-06-07 00:09:38 +0300

    footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,8 +17,8 @@ export default function Footer() {
   }));
 
   return (
-    <div className="relative bg-black pt-24 overflow-hidden bg-[linear-gradient(to_bottom,_#000000_0%,_#000000_60%,_#7D009B)] ">
-      {/* Shooting stars */}
+    <div className="relative bg-black pt-24 overflow-hidden bg-[linear-gradient(to_bottom,_#000000_0%,_#000000_60%,_#7D009B)]">
+      {/* Shooting Stars */}
       {stars.map((starItem, index) => (
         <img
           key={index}
@@ -32,55 +32,50 @@ export default function Footer() {
           }}
         />
       ))}
-      <div className="relative  text-white p-4 flex-wrap md:flex-nowrap flex justify-center">
-        <div className=" md:w-[30%] text-center my-12">
+
+      {/* Content */}
+      <div className="relative z-20 text-white p-4 flex-wrap md:flex-nowrap flex justify-center">
+        <div className="md:w-[30%] text-center my-12">
           <h3 className="text-3xl font-extrabold">Supernova</h3>
           <p className="py-12 text-xl">PARLLEL UNIVARESE FOR GAMERS</p>
-          <img className="m-auto" src={logo} alt="white logo" />
+          <img className="m-auto w-28" src={logo} alt="white logo" />
         </div>
-        <div className=" md:w-[30%] text-center my-12">
-          <h3 className="text-3xl font-extrabold ">Contact Information</h3>
-          <p className="py-12 text-xl "> info@supernova-gaming.com</p>
-          <p className="text-xl">+20 15 0393 3933 </p>
+
+        <div className="md:w-[30%] text-center my-12">
+          <h3 className="text-3xl font-extrabold">Contact Information</h3>
+          <p className="py-12 text-xl">info@supernova-gaming.com</p>
+          <p className="text-xl">+20 15 0393 3933</p>
         </div>
+
         <div className="md:w-[20%] text-center my-12">
-          <h3 className="text-3xl font-extrabold ">Follow Us</h3>
-          <div className="flex justify-center ">
-            <div className="m-2">
-             <a href="https://www.facebook.com/TheSuperNovaGamingg"> <img className="w-full block" src={fb} alt="facebook icon" /></a>
-            </div>
-            <div className="m-2">
-              <a href="https://www.instagram.com/thesupernovagaming/"><img className="w-full block" src={ig} alt="instagram icon" /></a>
-            </div>
-            <div className="m-2">
-             <a href="https://www.tiktok.com/@thesngshorts"><img className="w-full block" src={tiktok} alt="tiktok icon" /></a> 
-            </div>
-          </div>
-          <div className="flex justify-center mt-[-10px]">
-            <div className="m-2">
-              <a href="https://www.linkedin.com/company/supernovagaming"><img
-                className="w-full block"
-                src={linkedin}
-                alt="linkedin icon"
-              /></a>
-            </div>
-            <div className="m-2">
-             <a href="https://www.youtube.com/@thesupernovagamingg"> <img className="w-full block" src={yt} alt="youtibe icon" /></a>
-            </div>
-            <div className="m-2">
-             <a href="https://discord.com/invite/nbExCgThAv"> <img className="w-full block " src={discord} alt="discord icon" /></a>
-            </div>
+          <h3 className="text-3xl font-extrabold">Follow Us</h3>
+          <div className="grid grid-cols-3 gap-4 place-items-center mt-6">
+            <a href="https://www.facebook.com/TheSuperNovaGamingg" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={fb} alt="facebook" />
+            </a>
+            <a href="https://www.instagram.com/thesupernovagaming/" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={ig} alt="instagram" />
+            </a>
+            <a href="https://www.tiktok.com/@thesngshorts" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={tiktok} alt="tiktok" />
+            </a>
+            <a href="https://www.linkedin.com/company/supernovagaming" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={linkedin} alt="linkedin" />
+            </a>
+            <a href="https://www.youtube.com/@thesupernovagamingg" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={yt} alt="youtube" />
+            </a>
+            <a href="https://discord.com/invite/nbExCgThAv" target="_blank" rel="noopener noreferrer">
+              <img className="w-[100px] hover:scale-110 transition-transform" src={discord} alt="discord" />
+            </a>
           </div>
         </div>
       </div>
 
-      <div className="absolute top-[-600px] md:top-0 bottom-0 left-0 right-0 z-10">
-        {/* <img
-          src={footerbg}
-          alt="Color vector"
-          className="w-full h-[100%] object-cover"
-        /> */}
-      </div>
+      {/* Optional Background image (disabled) */}
+      {/* <div className="absolute top-[-600px] md:top-0 bottom-0 left-0 right-0 z-10">
+        <img src={footerbg} alt="Color vector" className="w-full h-full object-cover" />
+      </div> */}
     </div>
   );
 }
