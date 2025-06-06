5b5b72ac27f8c5d77aeae4a0387cdbcdac63a43c
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2025-06-07 00:34:21 +0300

    project page fix

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -52,32 +52,54 @@ export default function Projects() {
           /> */}
         </div>
       </div>
-      <div className=" ">
+      <div>
         <div className="flex flex-wrap gap-5 justify-around">
-          <div className="group relative rounded-lg border-4 m-7 border-[#7c009b59] text-white text-center w-[90vw] md:w-[40vw] h-[60vh] overflow-hidden">
-            <div className="absolute inset-0 flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out group-hover:opacity-0">
-              <h3 className="text-2xl md:text-4xl pb-5">356Games.net</h3>
+          <div className="group relative rounded-lg border-4 m-7 border-[#7c009b59] text-white text-center w-[90vw] md:w-[40vw] h-[50vh] md:h-[60vh] overflow-hidden">
+            <div className="absolute bottom-24 flex flex-col items-center ">
+              <h3 className="text-xl md:text-3xl border-0 relative font-bold">356Games.net</h3>
               <p className=" md:text-2xl p-5">
                 365Games – Bringing Gaming to Life Through Engaging Content
               </p>
             </div>
 
-            <div className="w-[300px] m-auto md:mt-28 mt-28 transition-opacity duration-500 ease-in-out opacity-0 group-hover:opacity-100">
+            <div className="md:w-[300px] w-[200px] mt-12 m-auto relative ">
               <img
                 src={games}
                 alt="Gaming Visual"
-                className="w-[full] h-full object-cover"
+                className="w-[full] h-full  bottom-0 object-cover"
               />
             </div>
             <Link to="/356games">
               <div>
-                <button className=" absolute bottom-0 self-start text-xl bg-[radial-gradient(circle,_rgba(255,255,255,0.3)_0%,_rgba(255,255,255,0)_100%)] text-white px-6 py-3 rounded-[10px] hover:shadow-[0_0_5px_3px_white] transition-all translate-x-[-50%] mb-5 border shadow-[0_0_5px_2px_rgba(255,255,255,0.5)] ">
+                <button className=" absolute bottom-0 self-start text-xl bg-[radial-gradient(circle,_rgba(255,255,255,0.3)_0%,_rgba(255,255,255,0)_100%)] text-white md:px-6 px-2 py-3 rounded-[10px] hover:shadow-[0_0_5px_3px_white] transition-all translate-x-[-50%] mb-5 border shadow-[0_0_5px_2px_rgba(255,255,255,0.5)] ">
                   Read More
                 </button>
               </div>
             </Link>
           </div>
-          <div className="group relative rounded-lg border-4 m-7 border-[#7c009b59] text-white text-center w-[90vw] md:w-[40vw] h-[60vh] overflow-hidden">
+          <div className="group relative rounded-lg border-4 m-7 border-[#7c009b59] text-white text-center w-[90vw] md:w-[40vw] h-[50vh] md:h-[60vh] overflow-hidden">
+           <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 text-center text-white ">
+  <h3 className="text-xl md:text-3xl font-bold">Arena PUBG Cup</h3>
+  <p className="   md:text-2xl mt-2">Bringing the Community Together</p>
+</div>
+
+
+            <div className="md:w-[300px] w-[200px] mt-0 m-auto relative ">
+              <img
+                src={arena}
+                alt="Gaming Visual"
+                className="w-[full] h-full  bottom-0 object-cover"
+              />
+            </div>
+            <Link to="/arena">
+              <div>
+                <button className=" absolute bottom-0 self-start text-xl bg-[radial-gradient(circle,_rgba(255,255,255,0.3)_0%,_rgba(255,255,255,0)_100%)] text-white md:px-6 px-2 py-3 rounded-[10px] hover:shadow-[0_0_5px_3px_white] transition-all translate-x-[-50%] mb-5 border shadow-[0_0_5px_2px_rgba(255,255,255,0.5)] ">
+                  Read More
+                </button>
+              </div>
+            </Link>
+          </div>
+          {/* <div className="group relative rounded-lg border-4 m-7 border-[#7c009b59] text-white text-center w-[90vw] md:w-[40vw] h-[60vh] overflow-hidden">
             <div className="absolute inset-0 flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out group-hover:opacity-0">
               <h3 className="text-2xl md:text-4xl pb-5">Arena PUBG Cup</h3>
               <p className=" md:text-2xl p-5">
@@ -99,7 +121,7 @@ export default function Projects() {
                 </button>
               </div>
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
       <div className=" bg-[#000000]  ">
